330d3d964053102ef00d2b52e7b2294c371b3742
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-02-15 16:15:48 -0300

    Create speech_text.py

---
+++ speech_text.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import speech_recognition as sr
+
+def listen_microphone():
+    microphone = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        microphone.adjust_for_ambient_noise(source)
+        st.info("Speak now...")
+        audio = microphone.listen(source)
+
+    try:
+        phrase = microphone.recognize_google(audio, language='en-US')
+        st.success("You said: {}".format(phrase))
+    except sr.UnknownValueError:
+        st.error("I didn't understand what you said.")
+    except sr.RequestError as e:
+        st.error("Could not request results from Google Speech Recognition service: {}".format(e))
+
+st.title("Speech-to-Text")
+st.write("Click the button and speak into your microphone to transcribe your speech to text.")
+
+if st.button("Start Recording"):
+    listen_microphone()
