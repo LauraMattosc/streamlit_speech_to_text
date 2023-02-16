cc825ba05d5bd520342ccfa549e03cccffc77c42
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-02-16 10:30:27 -0300

    Update speech_text.py

--- speech_text.py
+++ speech_text.py
@@ -1,6 +1,15 @@
 import speech_recognition as sr
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import words
 
-#Função para ouvir e reconhecer a fala
+# Download the words corpus if necessary
+nltk.download('words')
+
+# Get a set of English words from the corpus
+english_words = set(words.words())
+
+# Função para ouvir e reconhecer a fala
 def ouvir_microfone():
     #Habilita o microfone do usuário
     microfone = sr.Recognizer()
@@ -18,44 +27,41 @@ def ouvir_microfone():
         audio = microfone.listen(source)
         
     try:
-        
         #Passa a variável para o algoritmo reconhecedor de padroes
         frase = microfone.recognize_google(audio, language='pt-BR')
-        
-        #Retorna a frase pronunciada
         print("Você disse: " + frase)
         
-        #Lista de palavras a serem verificadas
+        # Lista de palavras a serem verificadas
         palavras = ["sereia", "árvore", "gato"]
-        
-        #Cria uma lista com as palavras reconhecidas
+
+        # Cria uma lista com as palavras reconhecidas
         palavras_reconhecidas = frase.split()
-        
-        #Inicia as variáveis de contagem
+
+        # Inicia as variáveis de contagem
         acertos = 0
         erros = 0
-        
-        #Verifica cada palavra reconhecida
+
+        # Verifica cada palavra reconhecida
         for palavra in palavras_reconhecidas:
-            #Se a palavra estiver na lista de palavras corretas, conta como acerto
+            # Se a palavra estiver na lista de palavras corretas, conta como acerto
             if palavra in palavras:
                 acertos += 1
                 print(palavra, "- correta")
-            #Se não estiver na lista, conta como erro
+            # Se não estiver na lista, conta como erro
             else:
                 erros += 1
                 print(palavra, "- incorreta")
-        
-        #Exibe a mensagem de resultado
+
+        # Exibe a mensagem de resultado
         if erros > 0:
             print("Você acertou", acertos, "palavra(s) e errou", erros, "palavra(s)")
         else:
             print("Parabéns, você acertou todas as palavras!")
         
     #Se nao reconheceu o padrao de fala, exibe a mensagem
-    except sr.UnknownValueError:
+    except sr.UnkownValueError:
         print("Não entendi")
-        
-    return frase
+    except sr.RequestError as e:
+        print("Não foi possível completar a requisição; {0}".format(e))
 
 ouvir_microfone()
